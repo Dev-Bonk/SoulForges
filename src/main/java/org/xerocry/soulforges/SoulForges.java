7851b23f3e8c9c763ce2853df2a32acbcd002c14
xerocry <goldmask000@gmail.com>
2023-03-06 00:17:53 +0400

    Updated modid

--- src/main/java/org/xerocry/soulforges/SoulForges.java
+++ src/main/java/org/xerocry/soulforges/SoulForges.java
@@ -17,11 +17,12 @@ import org.slf4j.Logger;
 
 import java.util.stream.Collectors;
 
-// The value here should match an entry in the META-INF/mods.toml file
-@Mod("SoulForges")
-public class SoulForges {
+import static org.xerocry.soulforges.SoulForges.MOD_ID;
 
-    // Directly reference a slf4j logger
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(MOD_ID)
+public class SoulForges {
+    public static final String MOD_ID = "soulforges";
     private static final Logger LOGGER = LogUtils.getLogger();
 
     public SoulForges() {
